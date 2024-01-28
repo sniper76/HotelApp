e3987cc656c39df8bd74d15be149443ac4ff5b09
sniper76 <snipers76@gmail.com>
2024-01-28 22:12:34 +0900

    Initial version of HotelApp generated by generator-jhipster@8.1.0

---
+++ src/main/java/com/hotel/app/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.hotel.app.repository;
+
+import com.hotel.app.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/com/hotel/app/IntegrationTest.java
@@ -0,0 +1,21 @@
+package com.hotel.app;
+
+import com.hotel.app.config.AsyncSyncConfiguration;
+import com.hotel.app.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { HotelApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
